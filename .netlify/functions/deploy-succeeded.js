d07c728b71e0beee6b60be153e987b31062ecd49
Joe Warren <joe.warren@nearform.com>
2019-03-12 18:09:09 +0000

    Add netlify function to run on deploy-succeeded

---
+++ .netlify/functions/deploy-succeeded.js
@@ -0,0 +1,44 @@
+const https = require('https')
+
+exports.handler = (event, context, callback) => {
+  console.log('event.body', event.body)
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: process.env.CIRCLECI_PROJECT_TOKEN
+  }
+
+  const postData = JSON.stringify({
+    build_parameters: {
+      CIRCLE_JOB: 'acceptance-tests',
+      ACCEPTANCE_URL: 'boo'
+    }
+  })
+
+  const req = https.request(
+    'https://circleci.com/api/v1.1/project/github/nearform/graphql-hooks/tree/netlify-trigger-circle',
+    options,
+    res => {
+      console.log(`STATUS: ${res.statusCode}`)
+      console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+      res.setEncoding('utf8')
+      res.on('data', chunk => {
+        console.log(`BODY: ${chunk}`)
+      })
+      res.on('end', () => {
+        console.log('No more data in response.')
+        callback()
+      })
+    }
+  )
+
+  req.on('error', e => {
+    console.error(`problem with request: ${e.message}`)
+  })
+
+  // write data to request body
+  req.write(postData)
+  req.end()
+}
